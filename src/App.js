9e0649cb20bf6559f12a235224fae550777093c2
Finemangsministern <emil_eriksson4@hotmail.com>
2021-06-17 20:45:12 +0200

    Adding html and jscode from aws amplify example

--- src/App.js
+++ src/App.js
@@ -18,8 +18,38 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <form>
+        <label>First Name :</label>
+        <input type="text" id="fName"/>
+        <label>Last Name :</label>
+        <input type="text" id="lName"/>
+        <button type="button" onclick="callAPI(document.getElementById('fName').value,document.getElementById('lName').value)">Call API</button>
+    </form>
     </div>
   );
+}
+
+// define the callAPI function that takes a first name and last name as parameters
+var callAPI = (firstName,lastName)=>{
+  // instantiate a headers object
+  var myHeaders = new Headers();
+  // add content type header to object
+  myHeaders.append("Content-Type", "application/json");
+  // using built in JSON utility package turn object to string and store in a variable
+  var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+  // create a JSON object with parameters for API call and store in a variable
+  var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+  };
+  // make API call with parameters and use promises to get response
+  fetch("https://fuzb5skoof.execute-api.eu-north-1.amazonaws.com/dev", requestOptions)
+  .then(response => response.text())
+  .then(result => alert(JSON.parse(result).body))
+  .catch(error => console.log('error', error));
 }
 
 export default App;
